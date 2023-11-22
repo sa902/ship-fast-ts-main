318b0b6163b84f09b043ebd5f750a5a2696b2c9e
sam <samanderson902@gmail.com>
2023-11-22 20:28:50 +1300

    added reverse

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,17 @@
+'use client'
 import Image from "next/image";
 import TestimonialsAvatars from "./TestimonialsAvatars";
 import config from "@/config";
-
-const Hero = () => {
+interface HeroProps{
+  reversed?:boolean
+}
+const Hero:React.FC<HeroProps> = ({reversed}) => {
+  const calculateReversed = () => {
+    console.log('inside our function here')
+    return reversed ? 'flex-row-reverse' : 'flex-row'
+  }
   return (
-    <section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
+    <section className={`max-w-7xl mx-auto bg-base-100 flex flex-col lg:${calculateReversed()} items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20`} >
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
         <a
           href="https://www.producthunt.com/posts/shipfast-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-shipfast&#0045;2"
